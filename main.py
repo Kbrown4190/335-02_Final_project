a0feba0a266b463dc9c9719be895a9731a7b31ea
Kbrown4190 <59777602+Kbrown4190@users.noreply.github.com>
2024-03-18 18:29:39 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ st.title("Early Detection of Heart Disease Based on Underlying Symptoms")
 
 dataset_path = 'Dataset/Heart.csv'
 
+st.subheader("Dataset Preview")
+st.dataframe(df.head())
+
 # Read the dataset into DataFrame
 df = pd.read_csv(dataset_path)
 
