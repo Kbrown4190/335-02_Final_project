c21f05f9afea3fb67c0992f21be34b6be22001eb
Kbrown4190 <brownky2@mail.gvsu.edu>
2024-04-14 14:36:22 -0400

    checkpoint 3
    added adaboost, SVM, and Decision Tree, needs extra reporting, and some more work on SVM

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import streamlit as st
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix, roc_auc_score, \
     fbeta_score
+from sklearn.svm import SVC
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
@@ -23,26 +25,51 @@ normalization_method = st.sidebar.radio(
     ("Z-Score", "Min-Max")
 )
 
+classifier_name = st.sidebar.selectbox(
+    'Select classifier',
+    ('Random Forest', 'AdaBoost', 'SVM', 'Decision Tree')
+)
+
 params = {}
+if classifier_name == 'Random Forest':
+    st.sidebar.subheader("Random Forest parameters")
+    params["criterion"] = st.sidebar.selectbox("Select Criterion", ["gini", "entropy"])
+    params["max_depth"] = st.sidebar.slider("Max Depth", 2, 10)
+    params["n_estimators"] = st.sidebar.slider("N Estimators", 10, 100)
+elif classifier_name == 'AdaBoost':
+    st.sidebar.subheader("AdaBoost parameters")
+    params["n_estimators"] = st.sidebar.slider("N Estimators", 50, 100)
+elif classifier_name == 'SVM':
+    st.sidebar.subheader("SVM parameters")
+    params["C"] = st.sidebar.slider("C", 0.01, 1.0)
+elif classifier_name == 'Decision Tree':
+    st.sidebar.subheader("Decision Tree parameters")
+    params["max_depth"] = st.sidebar.slider("Max Depth", 2, 15)
 
-st.sidebar.subheader("Random Forest parameters")
-params["criterion"] = st.sidebar.selectbox(label="Select Criterion", options=["gini", "entropy"])
-params["max_depth"] = st.sidebar.slider("Max Depth", 2, 10)
-params["n_estimators"] = st.sidebar.slider("N Estimators", 2, 50)
 
-
-def create_pipeline(normalization_method, params):
+def create_pipeline(normalization_method, classifier_name, params):
     if normalization_method == "Z-Score":
         scaler = StandardScaler()
-    else:  
+    else: 
         scaler = MinMaxScaler()
+
+    if classifier_name == 'Random Forest':
+        classifier = RandomForestClassifier(n_estimators=params.get("n_estimators", 10),
+                                            max_depth=params.get("max_depth"),
+                                            criterion=params.get("criterion", "gini"),
+                                            random_state=0)
+    elif classifier_name == 'AdaBoost':
+        classifier = AdaBoostClassifier(n_estimators=params.get("n_estimators", 50),
+                                        random_state=0)
+    elif classifier_name == 'SVM':
+        classifier = SVC(C=params.get("C", 1.0))
+    elif classifier_name == 'Decision Tree':
+        classifier = DecisionTreeClassifier(max_depth=params.get("max_depth"),
+                                            random_state=0)
 
     pipeline = Pipeline([
         ('normalizer', scaler),
-        ('classifier', RandomForestClassifier(n_estimators=params["n_estimators"],
-                                              max_depth=params["max_depth"],
-                                              criterion=params["criterion"],
-                                              random_state=0))
+        ('classifier', classifier)
     ])
     return pipeline
 
@@ -57,7 +84,7 @@ def train_and_evaluate_model(pipeline, X, y):
 if st.button("Train Model"):
     X = df.drop('target', axis=1)
     y = df['target']
-    pipeline = create_pipeline(normalization_method, params)
+    pipeline = create_pipeline(normalization_method,classifier_name, params)
     y_test, predictions = train_and_evaluate_model(pipeline, X, y)
     accuracy = accuracy_score(y_test, predictions)
     precision = precision_score(y_test, predictions)
@@ -65,7 +92,7 @@ if st.button("Train Model"):
     f1 = f1_score(y_test, predictions)
     f2 = fbeta_score(y_test, predictions,  beta=2)
 
-   
+    # Generating and displaying the confusion matrix
     conf_matrix = confusion_matrix(y_test, predictions)
     fig, ax = plt.subplots()
     sns.heatmap(conf_matrix, annot=True, fmt="d", cmap="Blues", ax=ax)
@@ -74,7 +101,7 @@ if st.button("Train Model"):
     ax.set_title('Confusion Matrix')
     st.pyplot(fig)
 
-
+    # Displaying metrics
     st.write(f"Accuracy: {accuracy:.2f}")
     st.write(f"Precision: {precision:.2f}")
     st.write(f"Recall: {recall:.2f}")
